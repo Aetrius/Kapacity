54aed5a837785237b4ea49f5bf1b923cf2c34921
Tyler Bennet <tylerwbennet@gmail.com>
2024-06-20 16:44:06 -0500

    adding additional fields for pods

--- kapacity-collector/kubernetesClient.go
+++ kapacity-collector/kubernetesClient.go
@@ -118,6 +118,76 @@ func getStatefulsetData(namespaces *v1.NamespaceList, clientSet *kubernetes.Clie
 
 }
 
+func getReplicaSet(namespaces *v1.NamespaceList, clientSet *kubernetes.Clientset) ([]PodInfo, error) {
+	var err error
+	var allNamespaceInfo []PodInfo
+
+	for _, ns := range namespaces.Items {
+		var replicaSets, err = clientSet.AppsV1().ReplicaSets(ns.Name).List(context.TODO(), metav1.ListOptions{})
+
+		if err != nil {
+			return allNamespaceInfo, err
+		}
+
+		for _, deploy := range replicaSets.Items {
+			owner := deploy.Labels["Owner"]
+			for _, c := range deploy.Spec.Template.Spec.Containers {
+				info := PodInfo{
+					Namespace:     ns.Name,
+					Type:          "ReplicaSet",
+					Name:          deploy.Name,
+					Owner:         owner,
+					PodCount:      *deploy.Spec.Replicas,
+					Container:     c.Name,
+					CPURequest:    c.Resources.Requests.Cpu().String(),
+					CPULimit:      c.Resources.Limits.Cpu().String(),
+					MemoryRequest: c.Resources.Requests.Memory().String(),
+					MemoryLimit:   c.Resources.Limits.Memory().String(),
+				}
+				allNamespaceInfo = append(allNamespaceInfo, info)
+			}
+		}
+	}
+
+	return allNamespaceInfo, err
+
+}
+
+func getDaemonSets(namespaces *v1.NamespaceList, clientSet *kubernetes.Clientset) ([]PodInfo, error) {
+	var err error
+	var allNamespaceInfo []PodInfo
+
+	for _, ns := range namespaces.Items {
+		var daemonSets, err = clientSet.AppsV1().DaemonSets(ns.Name).List(context.TODO(), metav1.ListOptions{})
+
+		if err != nil {
+			return allNamespaceInfo, err
+		}
+
+		for _, deploy := range daemonSets.Items {
+			owner := deploy.Labels["Owner"]
+			for _, c := range deploy.Spec.Template.Spec.Containers {
+				info := PodInfo{
+					Namespace:     ns.Name,
+					Type:          "DaemonSets",
+					Name:          deploy.Name,
+					Owner:         owner,
+					PodCount:      *&deploy.Status.NumberAvailable,
+					Container:     c.Name,
+					CPURequest:    c.Resources.Requests.Cpu().String(),
+					CPULimit:      c.Resources.Limits.Cpu().String(),
+					MemoryRequest: c.Resources.Requests.Memory().String(),
+					MemoryLimit:   c.Resources.Limits.Memory().String(),
+				}
+				allNamespaceInfo = append(allNamespaceInfo, info)
+			}
+		}
+	}
+
+	return allNamespaceInfo, err
+
+}
+
 func gatherKubernetesInfo() ([]PodInfo, error) {
 	var allContainerInfo []PodInfo
 	var err error
@@ -127,9 +197,13 @@ func gatherKubernetesInfo() ([]PodInfo, error) {
 	namespaces, _ := getNamespaceData(clientSet)
 	deployments, _ := getDeploymentData(namespaces, clientSet)
 	statefulsets, _ := getStatefulsetData(namespaces, clientSet)
+	replicasets, _ := getReplicaSet(namespaces, clientSet)
+	daemonsets, _ := getDaemonSets(namespaces, clientSet)
 
 	allContainerInfo = append(allContainerInfo, deployments...)
 	allContainerInfo = append(allContainerInfo, statefulsets...)
+	allContainerInfo = append(allContainerInfo, replicasets...)
+	allContainerInfo = append(allContainerInfo, daemonsets...)
 
 	return allContainerInfo, err
 
